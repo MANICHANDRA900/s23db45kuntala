9b52bb6cb2ae7433abf6a8cb46cdd0a4d623ba97
MANICHANDRA900 <143015834+MANICHANDRA900@users.noreply.github.com>
2023-11-08 18:59:13 -0600

    Implemented all lists

--- controllers/equipment.js
+++ controllers/equipment.js
@@ -1,21 +1,32 @@
 var Equipment = require('../models/equipment');
-// List of all Costumes
+// List of all Equipments
 exports.equipment_list = function(req, res) {
  res.send('NOT IMPLEMENTED: Equipment list');
 };
-// for a specific Costume.
+// for a specific Equipment.
 exports.equipment_detail = function(req, res) {
  res.send('NOT IMPLEMENTED: Equipment detail: ' + req.params.id);
 };
-// Handle Costume create on POST.
+// Handle Equipment create on POST.
 exports.equipment_create_post = function(req, res) {
  res.send('NOT IMPLEMENTED: Equipment create POST');
 };
-// Handle Costume delete form on DELETE.
+// Handle Equipment delete form on DELETE.
 exports.equipment_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: Equipment delete DELETE ' + req.params.id);
 };
-// Handle Costume update form on PUT.
+// Handle Equipment update form on PUT.
 exports.equipment_update_put = function(req, res) {
  res.send('NOT IMPLEMENTED: Equipment update PUT' + req.params.id);
 };
+// List of all Equipments
+exports.equipment_list = async function(req, res) {
+    try{
+    theEquipments = await Equipment.find();
+    res.send(theEquipments);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
