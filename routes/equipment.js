eac8e61f0b18fd0b3563a35e19b7d2923e2eb6c0
MANICHANDRA900 <143015834+MANICHANDRA900@users.noreply.github.com>
2023-11-15 18:01:05 -0600

    Added Page View for displaying one collection instance.

--- routes/equipment.js
+++ routes/equipment.js
@@ -4,3 +4,5 @@ var router = express.Router();
 /* GET costumes */
 router.get('/', equipment_controlers.equipment_view_all_Page );
 module.exports = router;
+/* GET detail costume page */
+router.get('/detail', equipment_controlers.equipment_view_one_Page);
