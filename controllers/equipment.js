13a5a6b9401165f5a2e8c6a85e068e51f86d7f40
MANICHANDRA900 <143015834+MANICHANDRA900@users.noreply.github.com>
2023-11-15 18:30:13 -0600

    Added  Page for update of one instance on a browser.

--- controllers/equipment.js
+++ controllers/equipment.js
@@ -115,4 +115,32 @@ exports.equipment_view_one_Page = async function(req, res) {
     res.send(`{'error': '${err}'}`);
     }
    };
+
+   // Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.equipment_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('equipmentcreate', { title: 'equipment Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+   // Handle building the view for updating a costume.
+// query provides the id
+exports.equipment_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await equipment.findById(req.query.id)
+    res.render('equipmentupdate', { title: 'equipment Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
    
--- routes/equipment.js
+++ routes/equipment.js
@@ -3,6 +3,10 @@ const equipment_controlers= require('../controllers/equipment');
 var router = express.Router();
 /* GET costumes */
 router.get('/', equipment_controlers.equipment_view_all_Page );
-module.exports = router;
 /* GET detail costume page */
 router.get('/detail', equipment_controlers.equipment_view_one_Page);
+/* GET create costume page */
+router.get('/create', equipment_controlers.equipment_create_Page);
+/* GET create update page */
+router.get('/update', equipment_controlers.equipment_update_Page);
+module.exports = router;
