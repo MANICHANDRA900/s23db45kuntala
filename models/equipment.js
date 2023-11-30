573f7e66c219cf428a57a532f675f933c27c0bb6
MANICHANDRA900 <143015834+MANICHANDRA900@users.noreply.github.com>
2023-11-29 21:52:52 -0600

    Added limit

--- models/equipment.js
+++ models/equipment.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose")
 const equipmentSchema = mongoose.Schema({
 equipment_type: String,
 condition: String,
-quantity: Number
+quantity: {
+    type: Number,
+    min: [0, 'The equipment quantity must be greater than or equal to 0'], 
+    max: [70000, 'The equipment quantity cannot exceed 70000'], 
+    required: [true, 'equipment quantity is required'] 
+}
 })
 module.exports = mongoose.model("equipment",equipmentSchema)
