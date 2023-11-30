acd09772ffb783fc07d0d0547a41c05b6bfb329b
MANICHANDRA900 <143015834+MANICHANDRA900@users.noreply.github.com>
2023-11-29 21:39:42 -0600

    updating the equipments

--- routes/equipment.js
+++ routes/equipment.js
@@ -1,5 +1,6 @@
 var express = require('express');
 const equipment_controlers= require('../controllers/equipment');
+var passport = require('passport');
 var router = express.Router();
 /* GET costumes */
 router.get('/', equipment_controlers.equipment_view_all_Page );
@@ -8,7 +9,22 @@ router.get('/detail', equipment_controlers.equipment_view_one_Page);
 /* GET create costume page */
 router.get('/create', equipment_controlers.equipment_create_Page);
 /* GET create update page */
-router.get('/update', equipment_controlers.equipment_update_Page);
+//router.get('/update', equipment_controlers.equipment_update_Page);
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ res.redirect("/login");
+ }
+ /* GET update costume page */
+router.get('/update', secured,
+equipment_controlers.equipment_update_Page);
+router.post('/login',passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+   });
 /* GET delete costume page */
 router.get('/delete', equipment_controlers.equipment_delete_Page);
 module.exports = router;
